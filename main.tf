b547bf9f10318a66f246947b26ed6742cfed0ae4
Imran Hussain <43928033+imran786110@users.noreply.github.com>
2022-12-01 14:11:54 +0100

    errors solved by removin g =

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data = "aws_vpc" "default" {
+data "aws_vpc" "default" {
   default = true
 }
 
@@ -29,14 +29,14 @@ resource "aws_instance" "blog" {
   }
 }
 
-resource = "aws_security_group" "blog" {
+resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow Http Https in, allow everything out"
 
   vpc_id = data.aws_vpc.default.id
 }
 
-resource = "aws_security_group_rule" "blog_http_in" {
+resource "aws_security_group_rule" "blog_http_in" {
   type        = "ingress"
   from_port   = 80
   to_port     = 80
@@ -46,7 +46,7 @@ resource = "aws_security_group_rule" "blog_http_in" {
   security_group_id  = aws_security_group.blog.id
 }
 
-resource = "aws_security_group_rule" "blog_https_in" {
+resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
   from_port   = 443
   to_port     = 443
@@ -56,7 +56,7 @@ resource = "aws_security_group_rule" "blog_https_in" {
   security_group_id  = aws_security_group.blog.id
 }
 
-resource = "aws_security_group_rule" "blog_everything_out" {
+resource "aws_security_group_rule" "blog_everything_out" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
